753d827bfae16d9b3fce5a1c4912510ecb86152e
rudy <rudypichola@gmail.com>
2019-10-05 13:56:10 -0500

    finished homepage UI

--- catastrophe/src/App.js
+++ catastrophe/src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import extractFramesFromVideo from './frameExtractor';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const App = () => {
+
+  const [video, setVideo] = useState('');
+
+  const extract = async (event) => {
+    setVideo(video+event.target.value);
   }
-}
 
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    const cors = 'https://cors-anywhere.herokuapp.com/';
+
+    let frames = await extractFramesFromVideo(`${cors}${video}`);
+
+    for(let i of frames) {
+      setTimeout(() => {
+        let img = document.getElementById('temp');
+        img.src = i;
+      }, 3000);
+    }
+  }
+
+  return (
+    <React.Fragment>
+      {/* App Bar */}
+      <div id="app-bar">
+        <h1 id="title">CATastrophe</h1>
+      </div>
+      <div id="content">
+        <form id="form" onSubmit={handleSubmit}>
+          <input id="input" value={video} type="text" name="video" onChange={extract}/>
+          <input type="submit"/>
+        </form>
+        <img src='' id="temp"/>
+      </div>
+    </React.Fragment>
+  );
+}
+ 
 export default App;
